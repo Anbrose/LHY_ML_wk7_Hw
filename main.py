cd94ed6fe0e80d6939623fe7f1d62ca2224a2c56
Anbrose <zemons@outlook.com>
2021-11-02 10:04:59 +0800

    removed eval part

--- main.py
+++ main.py
@@ -253,26 +253,4 @@ if __name__ == '__main__':
     print("Saving Model ...")
     model_save_dir = "saved_model"
     model.save_pretrained(model_save_dir)
-
-    print("Evaluating Test Set ...")
-
-    result = []
-
-    model.eval()
-    with torch.no_grad():
-        for data in tqdm(test_loader):
-            output = model(input_ids=data[0].squeeze(dim=0).to(device),
-                           token_type_ids=data[1].squeeze(dim=0).to(device),
-                           attention_mask=data[2].squeeze(dim=0).to(device))
-            result.append(evaluate(data, output))
-
-    result_file = "result.csv"
-    with open(result_file, 'w') as f:
-        f.write("ID,Answer\n")
-        for i, test_question in enumerate(test_questions):
-            # Replace commas in answers with empty strings (since csv is separated by comma)
-            # Answers in kaggle are processed in the same way
-            f.write(f"{test_question['id']},{result[i].replace(',', '')}\n")
-
-    print(f"Completed! Result is in {result_file}")
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
